e30d853b726bb210841ddad31a44547a5dc6ccb9
AlexisNichel <agranja@gmail.com>
2016-05-05 10:57:21 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var azure = require('azure-storage');
 var azure = require('azure-storage');
 var nconf = require('nconf');
+var multiparty = require('multiparty');
 nconf.env()
-     .file({ file: 'config.json', search: true });
+    .file({ file: 'config.json', search: true });
 var accountName = nconf.get("STORAGE_NAME");
 var accountKey = nconf.get("STORAGE_KEY");
 
@@ -14,17 +15,32 @@ var blobSvc = azure.createBlobService(accountName, accountKey);
 
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.post('/upload', function (req, res) {
-    var myStream = req;
-    var myStreamLength = req.length();
-            blobSvc.createBlockBlobFromStream('logs', myStream,myStreamLength, function(error) {
+    var form = new multiparty.Form();
+    form.parse(req);
+    console.log(form)
+    form.on('part', function (part) {
+        if (part.filename) {
+            var filename = part.filename;
+            var size = part.byteCount;
+
+            var onError = function (error) {
+                if (error) {
+                    res.send({ grrr: error });
+                }
+            }
+            blobSvc.createBlockBlobFromStream('logs', 'logs.log',part, size, function (error) {
                 if (error) {
                     res.send({ Grrr: error });
                 }
-    });
+            });
+        };
+    })
+
+
     form.parse(req);
     res.send('OK');
 });
